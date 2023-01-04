af0070ca747a50f791037fe597cda5b71b0cc803
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-01-04 20:07:20 +0500

    Indicators

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
+import Indicators from './components/Indicators';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Indicators />
       <Products />
     </div>
   );
---
+++ src/components/Indicators.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Indicators() {
+  return (
+    <div className='container'>
+      <h1>Ko'rsatkichlar</h1>
+    </div>
+  )
+}
+
+export default Indicators
