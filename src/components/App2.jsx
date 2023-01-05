3aa29d261eca4e3eb32fd9047559f542901e21bd
Abu _WEB <sodiqbek2002@gmail.com>
2023-01-05 10:49:05 +0500

    Add

---
+++ src/components/App2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Indicators from './Indicators'
+import Navbar from './Navbar'
+import Products from './Products'
+
+function App2() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Indicators />
+      <Products />
+    </div>
+  )
+}
+
+export default App2
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Styles/Loader.css'
+
+function Loader() {
+  return (
+    <div className='loader'>
+    </div>
+  )
+}
+
+export default Loader
